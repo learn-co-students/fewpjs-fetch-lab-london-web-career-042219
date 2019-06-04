4be0da511d5342d724157a28bd113764c2dbdf9e
Lauren Carne <oreocookie@hotmail.co.uk>
2019-06-04 17:10:12 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
@@ -14,3 +16,33 @@ function renderBooks(json) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+// const baseURL = "https://anapioficeandfire.com/api/"
+
+// renderBook = book => {
+//   const main = document.querySelector('main')
+//   const h2 = document.createElement('h2')
+//   h2.innerHTML = `<h2>${book.name}</h2>`
+//   main.appendChild(h2)
+// }
+
+// renderCharacter = character => {
+//   const main = document.querySelector('main')
+//   const h2 = document.createElement('h2')
+//   h2.innerHTML = `<h2>${character.name}</h2>`
+//   main.appendChild(h2)
+// }
+
+
+
+// fetchBooks(baseURL + "books/3").then(json => {
+//   renderBook(json)
+// })
+
+// fetchBooks(baseURL + "characters/1031").then(json => {
+//   renderCharacter(json)
+// })
+
+// fetchBooks(baseURL + "books/").then(json => {
+//   json.forEach
+// })
