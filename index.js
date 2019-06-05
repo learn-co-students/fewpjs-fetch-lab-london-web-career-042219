713c070ca0a27d0a4ad1ab5f18d2c29b9f5192a2
Zubair <zubair.maqsood6@gmail.com>
2019-06-05 17:24:27 +0100

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
-function fetchBooks() {
+function fetchBooks(bookid) {
+  fetch(`https://anapioficeandfire.com/api/books/${bookid}`)
+  .then(resp => {
+    console.log(resp);
+    return resp.json()
+
+  })
+  .then(json => {
+    console.log(json);
+  })
+
+  .catch(error =>  console.log(error));
+}
+
+
+
+
+function fetchCharachter(characterid) {
+  fetch(`https://anapioficeandfire.com/api/characters/${characterid}`)
+  .then(resp => {
+    console.log(resp)
+    return resp.json()
+  })
+
+  .then(json => {
+    console.log(json);
+  })
+
+  .catch(error =>  console.log(error));
+}
+
+
+
+
+function totalpages() {
+  fetch(`https://anapioficeandfire.com/api/books`)
+  .then(resp => {
+    console.log(resp);
+    return resp.json()
+
+  })
+  .then(json => {
+    console.log(json);
+  })
+
+  .catch(error =>  console.log(error));
+
+
 
 }
+
+
 
 function renderBooks(json) {
   const main = document.querySelector('main')
@@ -10,6 +59,8 @@ function renderBooks(json) {
     main.appendChild(h2)
   })
 }
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
