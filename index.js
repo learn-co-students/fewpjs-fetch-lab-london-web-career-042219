3442361bb7c91743ee516260c1a44e5afbd0eb8d
github username <github email address>
2019-06-06 14:39:00 +0100

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-function fetchBooks() {
+// To pass this lab, this function should include a fetch
+// request to the Game of Thrones API ('https://anapioficeandfire.com/api/books').
+// The returned response should be converted to JSON. 
 
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
+
+
+// To pass this lab, this function should include a fetch
+// request to the Game of Thrones API ('https://anapioficeandfire.com/api/books').
+// The returned response should be converted to JSON. Then, call the second function,
 
 function renderBooks(json) {
   const main = document.querySelector('main')
